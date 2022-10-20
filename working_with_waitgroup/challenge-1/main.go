aae03864ddb7e063009b2213f54441c8f224165f
shaf <shafeelinks@gmail.com>
2022-10-20 06:58:56 -0400

    Remove instructions comment

--- working_with_waitgroup/challenge-1/main.go
+++ working_with_waitgroup/challenge-1/main.go
@@ -19,13 +19,6 @@ func printMessage(wg *sync.WaitGroup) {
 var wg sync.WaitGroup
 
 func main() {
-
-	// challenge: modify this code so that the calls to updateMessage() on lines
-	// 28, 30, and 33 run as goroutines, and implement wait groups so that
-	// the program runs properly, and prints out three different messages.
-	// Then, write a test for all three functions in this program: updateMessage(),
-	// printMessage(), and main().
-
 	msg = "Hello, world!"
 
 	wg.Add(1)
