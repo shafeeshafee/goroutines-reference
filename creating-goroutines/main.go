8b7c60144e5f15a4c1d16d5de3f5a98de3713d65
shaf <shafeelinks@gmail.com>
2022-10-20 05:46:10 -0400

    Add wait group reference

---
+++ creating-goroutines/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	words := []string{
+		"panda",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"omega",
+		"zeta",
+		"theta",
+		"epsilon",
+	}
+
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+
+	time.Sleep(1 * time.Second)
+
+	wg.Add(1)
+	printSomething("This is the second thing to be printed.", &wg)
+
+}
